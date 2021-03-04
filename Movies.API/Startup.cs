f295bfd27f109467b93328b3509a33a2fd72a03a
Chika Onuchukwu <chuzksy@gmail.com>
2021-03-04 04:50:35 +0100

    Scaffolded the Movies entitys to create the Movies Controller

--- Movies.API/Startup.cs
+++ Movies.API/Startup.cs
@@ -1,16 +1,11 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Movies.API.Data;
 
 namespace Movies.API
 {
@@ -32,6 +27,9 @@ namespace Movies.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Movies.API", Version = "v1" });
             });
+
+            services.AddDbContext<MoviesAPIContext>(options =>
+                    options.UseInMemoryDatabase(("MoviesAPIContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
