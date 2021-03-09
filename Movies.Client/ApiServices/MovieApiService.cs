d71989447685357d25bf3a7abb433c502620064d
Chika Onuchukwu <chuzksy@gmail.com>
2021-03-09 06:52:39 +0100

    Convert Movies.Client MVC web app to consume Movies.API project

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,48 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+            movieList.Add(new Movie
+            {
+                Id = 1,
+                Genre = "Comic",
+                Title = "Chika",
+                Rating = "9.5",
+                ImageUrl = "image/url",
+                ReleaseDate = DateTime.Now,
+                Owner = "Chuzksy"
+            });
+
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
