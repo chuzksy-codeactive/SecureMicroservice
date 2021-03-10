609522119aa14a23779deeb2ad97917ac6ddd1c8
Chika Onuchukwu <chuzksy@gmail.com>
2021-03-10 06:43:19 +0100

    Register the DI and Pipeline of IS4 MVC UI Pages

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -84,12 +84,12 @@ namespace IdentityServer
                 new TestUser
                 {
                     SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
-                    Username = "mehmet",
-                    Password = "swn",
+                    Username = "chuzksy",
+                    Password = "password",
                     Claims = new List<Claim>
                     {
-                        new Claim(JwtClaimTypes.GivenName, "mehmet"),
-                        new Claim(JwtClaimTypes.FamilyName, "ozkaya")
+                        new Claim(JwtClaimTypes.GivenName, "chuzksy"),
+                        new Claim(JwtClaimTypes.FamilyName, "onuchukwu")
                     }
                 }
             };
