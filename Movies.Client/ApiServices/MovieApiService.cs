98d0963ac2825b8c4c443c6036fa0a208012dbf6
Chika Onuchukwu <chuzksy@gmail.com>
2021-03-29 14:41:41 +0100

    Consume protected Movie.API with HttpClientFactory from Movie.Client

--- Movies.Client/ApiServices/MovieApiService.cs
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -1,28 +1,86 @@
-﻿using Movies.Client.Models;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Http;
+using Movies.Client.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Movies.Client.ApiServices
 {
     public class MovieApiService : IMovieApiService
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MovieApiService(IHttpContextAccessor httpContextAccessor, IHttpClientFactory httpClientFactory)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _httpClientFactory = httpClientFactory;
+        }
+
         public async Task<IEnumerable<Movie>> GetMovies()
         {
-            var movieList = new List<Movie>();
-            movieList.Add(new Movie
-            {
-                Id = 1,
-                Genre = "Comic",
-                Title = "Chika",
-                Rating = "9.5",
-                ImageUrl = "image/url",
-                ReleaseDate = DateTime.Now,
-                Owner = "Chuzksy"
-            });
+            var httpClient = _httpClientFactory.CreateClient("MovieAPIClient");
 
-            return await Task.FromResult(movieList);
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                "/movies");
+
+            var response = await httpClient.SendAsync(
+                request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            return movieList;
+
+            //// 1. "retrieve" our api credentials. This must be registered on Identity Server!
+            //var apiClientCredentials = new ClientCredentialsTokenRequest
+            //{
+            //    Address = "https://localhost:5005/connect/token",
+
+            //    ClientId = "movieClient",
+            //    ClientSecret = "secret",
+
+            //    // This is the scope our Protected API requires. 
+            //    Scope = "movieAPI"
+            //};
+
+            //// creates a new HttpClient to talk to our IdentityServer (localhost:5005)
+            //var client = new HttpClient();
+
+            //// just checks if we can reach the Discovery document. Not 100% needed but..
+            //var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
+            //if (disco.IsError)
+            //{
+            //    return null; // throw 500 error
+            //}
+
+            //// 2. Authenticates and get an access token from Identity Server
+            //var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            //if (tokenResponse.IsError)
+            //{
+            //    return null;
+            //}
+
+            //// Another HttpClient for talking now with our Protected API
+            //var apiClient = new HttpClient();
+
+            //// 3. Set the access_token in the request Authorization: Bearer <token>
+            //client.SetBearerToken(tokenResponse.AccessToken);
+
+            //// 4. Send a request to our Protected API
+            //var response = await client.GetAsync("https://localhost:5001/api/movies");
+            //response.EnsureSuccessStatusCode();
+
+            //var content = await response.Content.ReadAsStringAsync();
+
+            //var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            //return movieList;
         }
 
         public Task<Movie> CreateMovie(Movie movie)
---
+++ Movies.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
@@ -0,0 +1,36 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http; 
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Movies.Client.HttpHandlers
+{
+    public class AuthenticationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ClientCredentialsTokenRequest _tokenRequest;
+
+        public AuthenticationDelegatingHandler(IHttpClientFactory httpClientFactory, ClientCredentialsTokenRequest tokenRequest)
+        {
+            _httpClientFactory = httpClientFactory;
+            _tokenRequest = tokenRequest;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var httpClient = _httpClientFactory.CreateClient("IDPClient");
+
+            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(_tokenRequest);
+            
+            if (tokenResponse.IsError)
+            {
+                throw new HttpRequestException("Something went wrong while requesting the access token");
+            }
+
+            request.SetBearerToken(tokenResponse.AccessToken);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
